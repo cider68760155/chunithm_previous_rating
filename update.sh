34e69c9d8f245877410270c764d43ef09c867529
cider68760155 <asq458@outlook.jp>
2022-07-11 21:20:25 +0900

    自動化スクリプトを追加

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+date=`date`
+cd /home/cider/chunithm/chunithm_previous_rating/python #フォルダは「cider68760155/chunithm_previous_rating/python」となるように読み替え
+source venv/bin/activate
+python get_id.py
+git checkout -b auto-update
+git add -A
+git commit -m "automatically updated at ${date}"
+git push
